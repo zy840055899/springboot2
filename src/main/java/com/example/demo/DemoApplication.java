c260d873d84c486523fdf2d28fd69744afeb00ba
zhengying <zhengyingaaa@163.com>
2019-04-24 17:04:39 +0800

    mapper和repository的关系

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,7 +6,9 @@ import org.springframework.cache.annotation.EnableCaching;
 
 
 @SpringBootApplication
-@EnableCaching              // 开启缓存
+@EnableCaching// 开启缓存
+//mapper接口的路径,不加也可以，dao上加上@mapper注解，加上的话可以用@repository注解，注入时候可以使用@Autowired
+//@MapperScan(basePackages = "com.example.demo.dao")
 public class DemoApplication {
 
     public static void main(String[] args) {
