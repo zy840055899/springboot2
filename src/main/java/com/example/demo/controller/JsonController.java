8ebad131734544024befae40d9e9b06d7416ee59
zhengying <zhengyingaaa@163.com>
2019-04-24 16:27:16 +0800

    redis的简单使用

--- src/main/java/com/example/demo/controller/JsonController.java
+++ src/main/java/com/example/demo/controller/JsonController.java
@@ -3,11 +3,11 @@ package com.example.demo.controller;
 import com.example.demo.dao.TestSqlDao;
 import com.example.demo.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.math.BigDecimal;
 
 @RestController
 public class JsonController {
@@ -18,9 +18,23 @@ public class JsonController {
     @Autowired
     private AccountService accountService;
 
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
     @RequestMapping("/testSql")
     String home() {
         return testSqlDao.testSql() + "";
+    }
+
+    /**
+     * redis测试 2019-4-24 11:29:39
+     *
+     * @return
+     */
+    @RequestMapping("/testRedis")
+    String testRedis() {
+        stringRedisTemplate.opsForValue().set("k5", "Springboot redis");
+        return stringRedisTemplate.opsForValue().get("k5");
     }
 
     @RequestMapping("/transferMoney")
