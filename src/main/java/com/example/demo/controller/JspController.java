d694120f228512b3bf1e184cafbb3594378f5532
zhengying <zhengyingaaa@163.com>
2019-04-29 15:53:01 +0800

    thymeleaf简单应用

--- src/main/java/com/example/demo/controller/JspController.java
+++ src/main/java/com/example/demo/controller/JspController.java
@@ -1,13 +1,16 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class JspController {
 
     @RequestMapping("/index")
-    String index() {
+    String index(Model model) {
+        model.addAttribute("hello", "hello world");
         return "index";
     }
 
