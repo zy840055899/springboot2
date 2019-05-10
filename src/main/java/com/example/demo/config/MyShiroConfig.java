2a946331c2ac470afb2dae6853cb846c5182c457
zy840055899 <840055899@qq.com>
2019-05-10 15:54:43 +0800

    shiro登录校验

--- src/main/java/com/example/demo/config/MyShiroConfig.java
+++ src/main/java/com/example/demo/config/MyShiroConfig.java
@@ -1,6 +1,5 @@
 package com.example.demo.config;
 
-import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
 import org.apache.shiro.realm.Realm;
 import org.apache.shiro.spring.web.config.DefaultShiroFilterChainDefinition;
 import org.apache.shiro.spring.web.config.ShiroFilterChainDefinition;
@@ -10,6 +9,18 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MyShiroConfig {
+
+    @Bean
+    public static DefaultAdvisorAutoProxyCreator getDefaultAdvisorAutoProxyCreator() {
+        DefaultAdvisorAutoProxyCreator creator = new DefaultAdvisorAutoProxyCreator();
+        /**
+         * setUsePrefix(false)用于解决一个奇怪的bug。在引入spring aop的情况下。
+         * 在@Controller注解的类的方法中加入@RequiresRole注解，会导致该方法无法映射请求，导致返回404。
+         * 加入这项配置能解决这个bug
+         */
+        creator.setUsePrefix(true);
+        return creator;
+    }
 
     /**
      * 注入自定义的realm，告诉shiro如何获取用户信息来做登录认证和授权
@@ -22,6 +33,7 @@ public class MyShiroConfig {
     /**
      * 这里统一做鉴权，即判断哪些请求路径需要用户登录，哪些请求路径不需要用户登录。
      * 这里只做鉴权，不做权限控制，因为权限用注解来做。
+     *
      * @return
      */
     @Bean
@@ -37,18 +49,6 @@ public class MyShiroConfig {
         //除了以上的请求外，其它请求都需要登录
         chain.addPathDefinition("/**", "authc");
         return chain;
-    }
-
-    @Bean
-    public static DefaultAdvisorAutoProxyCreator getDefaultAdvisorAutoProxyCreator() {
-        DefaultAdvisorAutoProxyCreator creator = new DefaultAdvisorAutoProxyCreator();
-        /**
-         * setUsePrefix(false)用于解决一个奇怪的bug。在引入spring aop的情况下。
-         * 在@Controller注解的类的方法中加入@RequiresRole注解，会导致该方法无法映射请求，导致返回404。
-         * 加入这项配置能解决这个bug
-         */
-        creator.setUsePrefix(true);
-        return creator;
     }
 
 }
